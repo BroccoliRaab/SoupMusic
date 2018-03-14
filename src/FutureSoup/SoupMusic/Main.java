9abecf6d7358368f33601fc4068d0eccb4a4340f
Unknown <rherlihy123@gmail.com>
2018-03-14 17:26:49 -0400

    Ignores /out, made color scheme, and symbols, and basic layout

--- src/FutureSoup/SoupMusic/Main.java
+++ src/FutureSoup/SoupMusic/Main.java
@@ -1,8 +1,12 @@
 package FutureSoup.SoupMusic;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -17,14 +21,39 @@ public class Main extends Application {
         //primaryStage.setScene(new Scene(root, DEFAULTWIDTH, DEFAULTHEIGHT));
 
         //BorderPane is a layout with a center, top, bottom, left, and right
-        //Left will have slection of by what to sort: Contr. Artist, Album Artist, Album, Genres, Years, Composers, ECT
+        //Left will have selection of by what to sort: Contr. Artist, Album Artist, Album, Genres, Years, Composers, ECT
         BorderPane mainPane = new BorderPane();
 
         Scene mainScene = new Scene(mainPane, DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
         primaryStage.setScene(mainScene);
 
+        mainScene.getStylesheets().add("Style.css");
+
         primaryStage.setTitle(TITLE);
+
+        HBox controls = new HBox();
+
+        VBox bodyV = new VBox();
+
+        bodyV.setId("bodyVBox");
+
+
+        mainPane.setCenter(bodyV);
+
+        VBox sortingPane = new VBox();
+
+        sortingPane.setPadding(new Insets(0, 200, 0, 0));
+        sortingPane.setId("sortingPane");
+        mainPane.setLeft(sortingPane);
+
+        controls.setPadding(new Insets(15, 12, 40, 12));
+        controls.setId("controlsBar");
+        //controls.setSpacing(10);
+        //controls.setAlignment(Pos.CENTER);
+
+
+        mainPane.setBottom(controls);
 
         primaryStage.show();
         
